11d901e1db9e0ac5fa77bfec8f267fd40318dda4
Oscar Linderholm <osm@recv.se>
2019-01-15 12:01:35 +0100

    Add workspace scripts

--- install.sh
+++ install.sh
@@ -4,8 +4,14 @@
 brew install awscli gnupg golang jq node pinentry-mac postgresql tmux vim
 brew cask install docker dropbox google-chrome iterm2 slack spotify tunnelblick
 brew services start postgresql
+
 tar -C /Applications -xzf $HOME/Documents/apps/1Password.app.tar.gz
+
 cp .bash_profile .gitconfig .tmux.conf .vimrc $HOME
+
 mkdir -p $HOME/.gnupg
 chmod 700 $HOME/.gnupg
 cp gpg-agent.conf $HOME/.gnupg
+
+mdir -p $HOME/bin
+cp workspace-* $HOME/bin
