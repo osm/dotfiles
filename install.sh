957001ba31d03cb6a507e432e4ac005997e4ce6f
Oscar Linderholm <osm@recv.se>
2019-01-16 06:22:36 +0100

    Install keepassx

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install awscli gnupg golang jq node pinentry-mac postgresql tmux vim
-brew cask install docker dropbox google-chrome iterm2 slack spotify tunnelblick
+brew cask install docker dropbox google-chrome iterm2 keepassx slack spotify tunnelblick
 brew services start postgresql
 
 tar -C /Applications -xzf $HOME/Documents/apps/1Password.app.tar.gz
