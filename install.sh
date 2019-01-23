bcbfec07cec1e8519d1bc885ba33a180375b948f
Oscar Linderholm <osm@recv.se>
2019-01-23 14:46:38 +0100

    Remove 1Password

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ brew install awscli consul gnupg golang jq node nomad pinentry-mac postgresql tm
 brew cask install docker dropbox google-chrome iterm2 keepassx slack spotify tunnelblick
 brew services start postgresql
 
-tar -C /Applications -xzf $HOME/Documents/apps/1Password.app.tar.gz
-
 cp .bash_profile .gitconfig .tmux.conf .vimrc $HOME
 
 mkdir -p $HOME/.gnupg
