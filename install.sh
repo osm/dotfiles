15436817433804352c0c5055beb722ed858c3b2b
Oscar Linderholm <osm@recv.se>
2019-10-18 13:59:49 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl -s -o "$HOME/.vimrc" https://raw.githubusercontent.com/osm/dotfiles/master/.vimrc
+curl -s -o "$HOME/.tmux.conf" https://raw.githubusercontent.com/osm/dotfiles/master/.tmux.conf-linux
